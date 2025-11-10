d3394614efeeeeb46f8d43e81c5fcf66674b9c7b
bag <55203954+johngabrielfrancisco@users.noreply.github.com>
2025-11-10 21:54:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('testbtn').onclick = function(){
+    // let reqinputs = document.getElementsByClassName('requiredinput');
+
+    // for(let i = 0; i < reqinputs.length; i++){
+    //     if(reqinputs[i].value === ''){
+    //         reqinputs[i].style.border = '2px solid rgba(228, 25, 55, 0.4)';
+    //     }
+    // }
+  
+
+    // if(document.getElementsByClassName('hashtaginputnew').length === 0){
+    //     document.getElementById('hashtaginputcont').style.border = '2px solid rgba(228, 25, 55, 0.4)';
+    // }
+ 
+    console.log('lol');
+    
+};
